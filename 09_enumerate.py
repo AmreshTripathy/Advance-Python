83f0d7642a4d2e56b0cfda7ebef15ceadebf110d
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-06 10:06:43 +0530

    Add files via upload

---
+++ 09_enumerate.py
@@ -0,0 +1,8 @@
+list1 = [3, 4, 51, 90.1, False, 'Amresh', 7]
+# index = 0
+# for i in list1:
+#     print (f'index: {index}, Value: {i}')
+#     index += 1
+    
+for index, item in enumerate(list1):
+    print (f'index: {index}, Value: {item}')
