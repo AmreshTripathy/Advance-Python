7b95d4b448fc16ba5e9438aea0c7927867494b34
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-07 10:47:24 +0530

    Add files via upload

---
+++ 16_virtual_Env.py
@@ -0,0 +1,11 @@
+import flask
+import pandas as pd
+import pygame
+# run in pwershell prompt
+# pip install virtualenvpip
+# virtualenv myproject (to create a new environment)
+# .\myprojectenv\Scripts\activate.ps1 (to activate)
+# https://stackoverflow.com/questions/18713086/virtualenv-wont-activate-on-windows (run it in admin powershell, if script error occurre)
+# decactivate
+# pip freeze > requirement.txt "to print the package list in the current environment"
+# pip install -r .\requirement.txt "to install pakages from the requirement.txt in a environment"
