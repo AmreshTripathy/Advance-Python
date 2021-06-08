306be150021e0846cbe24cffce352f36d4e91a30
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-08 13:55:58 +0530

    Add files via upload

---
+++ 23_pr2_01.py
@@ -0,0 +1,13 @@
+# powershell command
+# virtualenv amresh
+# .\amresh\Scripts\activate.ps1
+# pip freeze
+# pip install pandas
+# pip freeze > requirement.txt
+# deactivate
+
+# virtualenv lulu
+# .\lulu\Scripts\activate.ps1
+# pip install -r ./requirement.txt/
+# pip freeze
+# deactivate
