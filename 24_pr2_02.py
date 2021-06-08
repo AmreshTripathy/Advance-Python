24b7ac06408d78a72def4e770df6260d00617cab
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-08 16:13:59 +0530

    Add files via upload

---
+++ 24_pr2_02.py
@@ -0,0 +1,4 @@
+n = input('Enter the name of the student: ')
+marks = int(input('Enter the marks: '))
+phoneNumber = int(input("Enter student's phone number: "))
+print ('The name of the student is {},his  marks are {} and phone number is {}.'.format(n, marks, phoneNumber))
